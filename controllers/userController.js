d636d4364ab4c8718ce3399dddb8f3188b15844b
jooho7133 <jooho7133@gmail.com>
2021-04-09 22:04:00 +0900

    #7.6~8 Gihub Log In

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,17 +27,48 @@ export const postJoin = async (req, res, next) => {
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
+
 export const postLogin = passport.authenticate("local", {
   failureRedirect: routes.login,
   successRedirect: routes.home,
 });
 
-export const logout = (req, res) => {
-  // To Do: Process Log Out
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+  const {
+    _json: { id, avatar_url: avatarUrl, name, email },
+  } = profile;
+  try {
+    const user = await User.findOne({ email });
+    console.log(user);
+    if (user) {
+      user.githubId = id;
+      user.save();
+      return cb(null, user);
+    } else {
+      const newUser = await User.create({
+        email,
+        name,
+        githubId: id,
+        avatarUrl,
+      });
+      return cb(null, newUser);
+    }
+  } catch (error) {
+    return cb(error);
+  }
+};
+
+export const postGithubLogIn = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+export const logout = (req, res) => {
+  req.logout();
+  res.redirect(routes.home);
+};
+
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,16 @@
 import express from "express";
+import passport from "passport";
 import {
   getJoin,
   getLogin,
+  githubLogin,
   logout,
+  postGithubLogIn,
   postJoin,
   postLogin,
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
-import { onlyPublic } from "../middlewares";
+import { onlyPrivate, onlyPublic } from "../middlewares";
 import routes from "../routes";
 
 const globalRouter = express.Router();
@@ -20,7 +23,16 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
+globalRouter.get(routes.logout, onlyPrivate, logout);
 
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.gitHub, githubLogin);
+
+globalRouter.get(
+  routes.gitHubCallback,
+  passport.authenticate("github", {
+    failureRedirect: routes.login,
+  }),
+  postGithubLogIn
+);
 
 export default globalRouter;
