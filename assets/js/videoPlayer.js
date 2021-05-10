a35e51629fb825f6d36526af77614ec0e0556a7f
jooho7133 <jooho7133@gmail.com>
2021-05-10 20:52:23 +0900

    #9.3 Enter Fullscreen Exit Fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumnBtn = document.getElementById("jsVolumnBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -23,9 +24,24 @@ function handleVolumnClick() {
   }
 }
 
+function exitFullScreen() {
+  document.exitFullscreen();
+  fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScrnBtn.removeEventListener("click", exitFullScreen);
+  fullScrnBtn.addEventListener("click", goFullScreen);
+}
+
+function goFullScreen() {
+  videoContainer.requestFullscreen();
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScrnBtn.removeEventListener("click", goFullScreen);
+  fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumnBtn.addEventListener("click", handleVolumnClick);
+  fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
